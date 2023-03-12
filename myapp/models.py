b4a81645b57c35ebdb331d1a795b9bfb1f19b9e8
Longgz <88452277+Longgz@users.noreply.github.com>
2023-03-12 17:54:48 +0700

    v2-update

--- myapp/models.py
+++ myapp/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from .validators import validate_file_extension
+from .validators import validate_file_extension,file_size
 
 
 class Image(models.Model):
-    photo = models.FileField(upload_to='myimage', validators=[validate_file_extension])
+    photo = models.FileField(upload_to='myimage', validators=[validate_file_extension, file_size])
     date = models.DateTimeField(auto_now_add=True)
     note = models.CharField(max_length=200, default='', null=True, blank=True)
     # category = models.CharField(max_length=150, null=True, blank=True)
